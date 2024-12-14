1f9606518e7b2de2f46150e94d907b0d8a909afb
Isma05 <ismawatikalisat@gmail.com>
2024-12-14 17:04:45 +0700

    style: color style section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -3,8 +3,7 @@ export default function AboutSection() {
     <section className="mt-16 md:mt-0 container text-center w-full flex flex-col space-y-10 justify-center items-center" id="about">
       <h2 className="uppercase font-medium text-[var(--primary-color)] text-5xl">About</h2>
       <p className="text-xl uppercase font-medium md:max-w-2xl">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur, tempora eligendi aliquid odit accusamus ullam dolorem fuga velit doloremque porro neque eos a, quibusdam at veniam itaque laboriosam accusantium nobis autem nam
-        possimus sunt? Consequuntur obcaecati rerum beatae nobis debitis ut distinctio sequi animi, corrupti nisi
+      Busy Master game is an otome game with Chinese art style developed by BluStar Games Limited. Players will play as the main character who plays the role of managing a broken house and becoming its master. Here players will meet various love interests who are voiced by famous Chinese voice actors.
       </p>
     </section>
   );
