e5288bb60cf648343d3c5477d3044887642d7d1b
Isma05 <ismawatikalisat@gmail.com>
2024-12-19 14:21:30 +0700

    style: color style section

--- src/components/Skills/DATA_SKILLS.ts
+++ src/components/Skills/DATA_SKILLS.ts
@@ -2,30 +2,30 @@ import { ISkills } from './SkillsCard';
 
 export const DATA_SKILLS: ISkills[] = [
   {
-    title: 'Javascript',
+    title: 'Huo',
     tag: 'Frontend',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ',
+    desc: 'Huo or fire element is the spirit with the most powerful seat in souyong college. People who have this spirit are usually very respected and revered',
     img: '/tech/reactjs.png',
     link: '/',
   },
   {
-    title: 'Javascript',
+    title: 'Shui',
     tag: 'Frontend',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo rerum architecto velit quo ex beatae cumque quibusdam totam dolores laudantium voluptatem, doloremque nobis sapiente labore aperiam nulla blanditiis quod dolore',
+    desc: 'Shui or water element is an element that describes calmness in life. People who have this element are usually calm in facing any situation.',
     img: '/tech/node.png',
     link: '/',
   },
   {
-    title: 'Javascript',
+    title: 'Feng',
     tag: 'Frontend',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo rerum architecto velit quo ex beatae cumque quibusdam totam dolores laudantium voluptatem, doloremque nobis sapiente labore aperiam nulla blanditiis quod dolore',
+    desc: 'Feng or wind element is a spirit that describes the surrounding nature. Usually people who have this spirit tend to be hard workers and never give up.',
     img: '/tech/next.png',
     link: '/',
   },
   {
-    title: 'Javascript',
+    title: 'Tudi',
     tag: 'Frontend',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo rerum architecto velit quo ex beatae cumque quibusdam totam dolores laudantium voluptatem, doloremque nobis sapiente labore aperiam nulla blanditiis quod dolore',
+    desc: 'Tudi or earth is an elemental spirit that represents strength and resilience. People who have this element tend to have a strong protective nature.',
     img: '/tech/tailwind.png',
     link: '/',
   },
