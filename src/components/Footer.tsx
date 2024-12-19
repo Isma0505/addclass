61e5d3a2a5e96c2d516b7b8f6bba34c89201a785
Isma05 <ismawatikalisat@gmail.com>
2024-12-19 15:27:53 +0700

    style: color style section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -66,7 +66,7 @@ export default function Footer() {
 
         <ul className="mt-12 flex justify-center gap-6 md:gap-8">
           <li>
-            <a href="#" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
+            <a href="https://www.facebook.com/?locale=id_ID" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
               <span className="sr-only">Facebook</span>
               <svg className="size-7" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                 <path
@@ -79,7 +79,7 @@ export default function Footer() {
           </li>
 
           <li>
-            <a href="#" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
+            <a href="https://www.instagram.com/accounts/login/" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
               <span className="sr-only">Instagram</span>
               <svg className="size-7" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                 <path
@@ -92,7 +92,7 @@ export default function Footer() {
           </li>
 
           <li>
-            <a href="#" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
+            <a href="https://x.com/i/flow/login" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
               <span className="sr-only">Twitter</span>
               <svg className="size-7" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -101,7 +101,7 @@ export default function Footer() {
           </li>
 
           <li>
-            <a href="#" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
+            <a href="https://github.com/" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
               <span className="sr-only">GitHub</span>
               <svg className="size-7" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                 <path
@@ -114,7 +114,7 @@ export default function Footer() {
           </li>
 
           <li>
-            <a href="#" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
+            <a href="https://dribbble.com/tags/webdesign" rel="noreferrer" target="_blank" className="text-gray-700 transition hover:text-gray-700/75">
               <span className="sr-only">Dribbble</span>
               <svg className="size-7" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                 <path
