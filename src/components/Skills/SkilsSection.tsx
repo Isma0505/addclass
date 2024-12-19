f593af1824d9a9c8e0e05f3ef67ed1532df257fe
Isma05 <ismawatikalisat@gmail.com>
2024-12-19 15:40:17 +0700

    boring today

--- src/components/Skills/SkilsSection.tsx
+++ src/components/Skills/SkilsSection.tsx
@@ -4,7 +4,7 @@ import ProjectCard from './SkillsCard';
 export default function SkilsSection() {
   return (
     <section className=" w-full flex flex-col space-y-10 justify-center items-center" id="skils">
-      <h2 className="text-center uppercase font-medium text-[var(--primary-color)] text-5xl">Project</h2>
+      <h2 className="text-center uppercase font-medium text-[var(--primary-color)] text-5xl">Skills</h2>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-5 md:gap-10 container">
         {DATA_SKILLS.map((data, idx) => (
           <ProjectCard data={data} key={idx} />
